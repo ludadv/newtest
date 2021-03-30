1d78c1bf5225a0263698c52a9685a31aff9a51ea
Luda <luda50177@gmail.com>
2021-03-30 17:07:10 +0300

    create cart

---
+++ test2.js
@@ -0,0 +1,61 @@
+/* 4. Создать массив аудиторий академии. Объект-аудитория состоит из названия, количества посадочных мест (от 10 до 20) и названия факультета, для которого она предназначена. Написать несколько функций для работы с ним^
+
+Вывод на экран всех аудиторий;
+Вывод на экран аудиторий для указанного факультета;
+Вывод на экран только тех аудиторий, которые подходят для переданной группы. Объект-группа состоит из названия, количества студентов и названия факультета;
+Функция сортировки аудиторий по количеству мест;
+Функция сортировки аудиторий по названию (по алфавиту).
+ */
+
+$(document).ready(function() {
+    let audienceList = [
+        {
+            name: 'a',
+            quantity: 10,
+            faculty: 'economy',
+        },
+        {
+            name: 'c',
+            quantity: 26,
+            faculty: 'economy',
+        },
+        {
+            name: 'e',
+            quantity: 23,
+            faculty: 'journalism',
+        },
+        {
+            name: 'c',
+            quantity: 20,
+            faculty: 'economy',
+        },
+        {
+            name: 'd',
+            quantity: 25,
+            faculty: 'journalism',
+        },
+        {
+            name: 'd',
+            quantity: 28,
+            faculty: 'journalism',
+        },
+    ];
+    let group = {
+        name: 'd',
+        quantityStudents: 19,
+        faculty: 'economy',
+    }
+
+    function outAudienceList(list) {
+        console.log(list);
+    }
+
+    function outFaculty(list) {
+        let item = list.filter( currentValue => currentValue.faculty == 'journalism');
+        console.log(item);
+    }
+
+
+    outAudienceList(audienceList);
+    outFaculty(audienceList);
+});
